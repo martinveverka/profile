d803fad68fe4c4f4c34643097296e3d4ccde803b
Martin Veverka <martin@veverka.eu>
2016-11-27 21:57:59 +0100

    enhanced oh-my-zsh theme update (automatically change ZSH_THEME in .zshrc)

--- tools/update-theme.sh
+++ tools/update-theme.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 
+# download custom theme
 curl -fsSL -o ~/.oh-my-zsh/themes/mv.zsh-theme https://raw.githubusercontent.com/martinveverka/profile/master/data/.oh-my-zsh/themes/mv.zsh-theme
+
+# change to this theme
+sed -i '/ZSH_THEME/c\ZSH_THEME="mv"' ~/.zshrc
