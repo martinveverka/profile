f749334079141843216c406ef1547cfd19c79210
Martin Veverka <martin@veverka.eu>
2018-01-09 13:56:07 +0100

    disables zsh plugins update for now

--- tools/update-theme.sh
+++ tools/update-theme.sh
@@ -6,13 +6,15 @@ curl -fsSL -o ~/.oh-my-zsh/themes/mv.zsh-theme https://raw.githubusercontent.com
 # change to this theme
 sed -i '/ZSH_THEME/c\ZSH_THEME="mv"' ~/.zshrc
 
-# set plugins
-sed -i '/^plugins=/c\plugins=(git ssh-agent)' ~/.zshrc
+## TODO: add ssh-agent plugin with settings manually when needed
 
-# append ssh-agent plugin configuration
-if ! grep -q "# mv plugins:ssh-agent settings" ~/.zshrc; then
-	echo '' >> ~/.zshrc
-	echo '# mv plugins:ssh-agent settings' >> ~/.zshrc
-	echo '#zstyle :omz:plugins:ssh-agent agent-forwarding on' >> ~/.zshrc
-	echo 'zstyle :omz:plugins:ssh-agent lifetime 4h' >> ~/.zshrc
-fi
+## set plugins
+#sed -i '/^plugins=/c\plugins=(git ssh-agent)' ~/.zshrc
+
+## append ssh-agent plugin configuration
+#if ! grep -q "# mv plugins:ssh-agent settings" ~/.zshrc; then
+#	echo '' >> ~/.zshrc
+#	echo '# mv plugins:ssh-agent settings' >> ~/.zshrc
+#	echo '#zstyle :omz:plugins:ssh-agent agent-forwarding on' >> ~/.zshrc
+#	echo 'zstyle :omz:plugins:ssh-agent lifetime 4h' >> ~/.zshrc
+#fi
