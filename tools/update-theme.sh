01cf4b3da7f6671a21228d9e4bb82377390fb869
Martin Veverka <martin@veverka.eu>
2024-05-29 15:34:27 +0200

    Update update-theme.sh

--- tools/update-theme.sh
+++ tools/update-theme.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
 # download custom theme
-curl -fsSL -o ~/.oh-my-zsh/themes/mv.zsh-theme https://raw.githubusercontent.com/martinveverka/profile/master/data/.oh-my-zsh/themes/mv.zsh-theme
+#curl -fsSL -o ~/.oh-my-zsh/themes/mv.zsh-theme https://raw.githubusercontent.com/martinveverka/profile/master/data/.oh-my-zsh/themes/mv.zsh-theme
+wget https://raw.githubusercontent.com/martinveverka/profile/master/data/.oh-my-zsh/themes/mv.zsh-theme -O ~/.oh-my-zsh/themes/mv.zsh-theme
 
 # change to this theme
 sed -i '/ZSH_THEME/c\ZSH_THEME="mv"' ~/.zshrc
